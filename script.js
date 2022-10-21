9b868f3506a88d6d0ec7c1b9527f62d4ef3529e7
Tarique <anwartarique408@gmail.com>
2022-10-21 13:02:17 +0530

    Added changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var typed = new Typed(".auto-type", {
+   strings: ["Java Backend Developer.","Problem Solver.", "Bodybuilding Enthusiast.","Basketball Enthusiast." ],
+   typeSpeed: 100,
+   backSpeed: 100,
+   loop: true
+})
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
