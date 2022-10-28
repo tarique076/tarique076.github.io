ab9627d0dd4a30f52e8dee85918542b61f71c9df
Tarique <anwartarique408@gmail.com>
2022-10-28 20:12:24 +0530

    git stats and project section

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+//Typing-text.
 var typed = new Typed(".auto-type", {
    strings: ["Java Backend Developer.","Problem Solver.", "Bodybuilding Enthusiast.","Basketball Enthusiast." ],
    typeSpeed: 100,
    backSpeed: 100,
    loop: true
 })
-
+//-----------Navbar--------------------------------
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
    header.classList.toggle("sticky", this.window.scrollY>0); 
 })
+
+
+// --------------GitHub-Calender--------------------
+var username = "tarique076";
+GitHubCalendar(".calendar", "tarique076", { responsive: true });
+
+// Use a proxy
+GitHubCalendar(".calendar", "tarique076", {
+   proxy (username) {
+     return fetch(`https://your-proxy.com/github?user=${username}`)
+   }
+}).then(r => r.text(username))
